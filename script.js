18981154793ae360296ed2d20cd50c6a032c5437
derekbache <derek.bache@gmail.com>
2023-03-30 17:22:47 -0700

    Implement add, subtract, multiply and divide functionality

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let firstNumber;
 let secondNumber;
-let operator;
+let firstNumberText = "";
+let secondNumberText = "";
+let operator = "";
+let operatorText;
+let display = document.getElementById("display");
+let displayText;
 
 function add(num1, num2) {
   let total = num1 + num2;
@@ -20,13 +25,48 @@ function divide(num1, num2) {
 }
 function operate(operator, firstNumber, secondNumber) {
   switch (operator) {
-    case add:
-      return add(firstNumber, secondNumber);
-    case subtract:
-      return subtract(firstNumber, secondNumber);
-    case multiply:
-      return multiply(firstNumber, secondNumber);
-    case divide:
-      return divide(firstNumber, secondNumber);
+    case "+":
+      total = add(firstNumber, secondNumber);
+      return total;
+    case "-":
+      total = subtract(firstNumber, secondNumber);
+      return total;
+    case "*":
+      total = multiply(firstNumber, secondNumber);
+      return total;
+    case "/":
+      total = divide(firstNumber, secondNumber);
+      return total;
   }
+}
+function addToDisplay(character) {
+  if (character == "+") {
+    operator = "+";
+  } else if (character == "-") {
+    operator = "-";
+  } else if (character == "*") {
+    operator = "*";
+  } else if (character == "/") {
+    operator = "/";
+  } else if (!operator) {
+    firstNumberText += character;
+  } else {
+    secondNumberText += character;
+  }
+
+  updateDisplayInnerText();
+}
+
+function equals() {
+  firstNumber = parseFloat(firstNumberText);
+  secondNumber = parseFloat(secondNumberText);
+  operate(operator, firstNumber, secondNumber);
+  firstNumberText = total;
+  operator = "";
+  secondNumberText = "";
+  updateDisplayInnerText();
+}
+
+function updateDisplayInnerText() {
+  display.innerText = firstNumberText + " " + operator + " " + secondNumberText;
 }
